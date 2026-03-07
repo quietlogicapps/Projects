8a8c26aa2e6097c7279afa5b41f0622a40ed2bd7
quietlogicapps <quietlogicapps@gmail.com>
2026-03-07 16:51:55 +0200

    T15 security screen wired and accessible from home menu

---
+++ app/src/main/java/com/quietlogic/allisok/security/SecurityActivity.kt
@@ -0,0 +1,15 @@
+package com.quietlogic.allisok.ui.security
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.quietlogic.allisok.R
+
+class SecurityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_security)
+
+    }
+}
--- app/src/main/java/com/quietlogic/allisok/ui/home/HomeActivity.kt
+++ app/src/main/java/com/quietlogic/allisok/ui/home/HomeActivity.kt
@@ -10,9 +10,10 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.quietlogic.allisok.R
-import com.quietlogic.allisok.ui.contacts.ContactsActivity
 import com.quietlogic.allisok.ui.care.CareActivity
+import com.quietlogic.allisok.ui.contacts.ContactsActivity
 import com.quietlogic.allisok.ui.info.InfoActivity
+import com.quietlogic.allisok.ui.security.SecurityActivity
 
 class HomeActivity : AppCompatActivity() {
 
@@ -25,7 +26,6 @@ class HomeActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         supportActionBar?.setDisplayShowTitleEnabled(false)
-
         toolbar.overflowIcon?.setTint(Color.WHITE)
 
         val contactsButton = findViewById<Button>(R.id.buttonContacts)
@@ -46,45 +46,25 @@ class HomeActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-
-        menuInflater.inflate(R.menu.menu_home, menu)
-
+        menuInflater.inflate(R.menu.home_menu, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
         when (item.itemId) {
-
-            R.id.actionSecurity -> {
+            R.id.menu_security -> {
+                startActivity(Intent(this, SecurityActivity::class.java))
                 return true
             }
 
-            R.id.actionLanguage -> {
-                return true
-            }
+            R.id.menu_language -> return true
+            R.id.menu_export -> return true
+            R.id.menu_import -> return true
+            R.id.menu_history -> return true
 
-            R.id.actionExport -> {
-                return true
-            }
-
-            R.id.actionImport -> {
-                return true
-            }
-
-            R.id.actionHistory -> {
-                return true
-            }
-
-            R.id.actionPrivacy -> {
-                return true
-            }
-
-            R.id.actionMoreApps -> {
-
+            R.id.menu_more_apps -> {
                 val uri = Uri.parse("https://play.google.com/store/apps/dev?id=QuietLogic")
                 startActivity(Intent(Intent.ACTION_VIEW, uri))
-
                 return true
             }
         }
