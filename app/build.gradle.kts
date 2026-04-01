e4c3ff210c6dadccfaaf654010fc1293c4c682b5
quietlogicapps <quietlogicapps@gmail.com>
2026-04-01 16:12:52 +0300

    remove READ_CONTACTS permission for Play Console compliance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,13 +6,13 @@ plugins {
 
 android {
     namespace = "com.quietlogic.allisok"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.quietlogic.allisok"
         minSdk = 28
-        targetSdk = 34
-        versionCode = 1
+        targetSdk = 35
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
