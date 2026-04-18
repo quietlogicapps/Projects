b31c9cc3636a8f3b83112516a5314676692dd681
quietlogicapps <quietlogicapps@gmail.com>
2026-04-18 22:43:03 +0300

    release: version 1.9 (AAB build)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.quietlogic.allisok"
         minSdk = 28
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.8"
+        versionCode = 9
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
