4ddbeeb4a250c88355138a421ebc2cc3f785f347
quietlogicapps <quietlogicapps@gmail.com>
2026-04-25 10:48:56 +0300

    gradle clean build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.quietlogic.allisok"
         minSdk = 28
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.11"
+        versionCode = 12
+        versionName = "1.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
