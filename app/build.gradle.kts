2e2db1f9d3bcda408b4e4bd589e1d82b60932067
quietlogicapps <quietlogicapps@gmail.com>
2026-05-12 16:01:19 +0300

    Build 16 - Fix TimePicker white divider line + deprecate statusBarColor via WindowCompat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.quietlogic.allisok"
         minSdk = 28
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.15"
+        versionCode = 16
+        versionName = "1.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
