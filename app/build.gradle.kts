101d1c46b797b64b128f430209c9ced2d32efaaa
quietlogicapps <quietlogicapps@gmail.com>
2026-05-19 14:27:40 +0300

    Version 1.18 - Fix 16KB page size support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.quietlogic.allisok"
         minSdk = 28
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.17"
+        versionCode = 18
+        versionName = "1.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
