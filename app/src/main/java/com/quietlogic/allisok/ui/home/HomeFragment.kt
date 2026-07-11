605a027819e9d4a3d4b8c7cdc2cbd5a8c952a99c
quietlogicapps <quietlogicapps@gmail.com>
2026-07-11 20:50:46 +0300

    Redesign home action cards

--- app/src/main/java/com/quietlogic/allisok/ui/home/HomeFragment.kt
+++ app/src/main/java/com/quietlogic/allisok/ui/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.material.button.MaterialButton
 import com.quietlogic.allisok.R
 import com.quietlogic.allisok.ui.care.CareActivity
 import com.quietlogic.allisok.ui.contacts.ContactsActivity
@@ -25,9 +24,9 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val contactsButton = view.findViewById<MaterialButton>(R.id.buttonContacts)
-        val careButton = view.findViewById<MaterialButton>(R.id.buttonCare)
-        val infoButton = view.findViewById<MaterialButton>(R.id.buttonInfo)
+        val contactsButton = view.findViewById<View>(R.id.buttonContacts)
+        val careButton = view.findViewById<View>(R.id.buttonCare)
+        val infoButton = view.findViewById<View>(R.id.buttonInfo)
 
         contactsButton.setOnClickListener {
             startActivity(Intent(requireContext(), ContactsActivity::class.java))
