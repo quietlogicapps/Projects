46b2aca5b59231fd049ce43c2353cd10ee6c808b
quietlogicapps <quietlogicapps@gmail.com>
2026-07-16 09:06:47 +0300

    Prepare v1.24 release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.quietlogic.allisok"
         minSdk = 28
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.23"
+        versionCode = 24
+        versionName = "1.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
